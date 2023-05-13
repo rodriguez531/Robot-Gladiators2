20e7c5d31575e895fbe8d6093bbfaf92dd89e166
rodriguez531 <alexr5408@gmail.com>
2023-05-13 11:20:00 -0400

    added shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -97,7 +97,17 @@ for (var i = 0; i < enemyNames.length; i++) {
 
     // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
     fight(pickedEnemyName);
+
+    //if we're not at the last enemy of in the array
+    if (playerHealth > 0 && i < enemyNames.length -1 ){
+    //ask if player wants to use the store before next round
+    var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+    //if yes, take them to the store() function
+      shop();
+    }
   }
+
+
   // if player isn't alive, stop the game
   else {
     window.alert('You have lost your robot in battle! Game Over!');
@@ -112,6 +122,54 @@ endGame();
 //play again
 startGame();
 debugger;
+};
+
+var shop = function(){
+//ask player what they'd like to do
+var shopOptionPrompt = window.prompt(
+  "Would you like to REFILL your healt, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or, 'LEAVE' to make to make a choice."
+);
+
+// use switch to carry out action
+switch (shopOptionPrompt) {
+  case "REFILL": // newcase
+  case "refill":
+  if (playerMoney >= 7) {
+    window.alert("Refilling player's health by 20 for 7 dollars.");
+
+    // increase health and decrease money
+    playerHealth = playerHealth + 20;
+    playerMoney = playerMoney - 7;
+  }
+  else {
+    window.alert("You don't have enough money!");
+  }
+
+  break;
+case "UPGRADE": // new case
+case "upgrade":
+  if (playerMoney >= 7) {
+    window.alert("Upgrading player's attack by 6 for 7 dollars.");
+
+   // increase attack and decrease money
+    playerAttack = playerAttack + 6;
+    playerMoney = playerMoney - 7;
+  }
+  else {
+    window.alert("You don't have enough money!");
+  }
+  break;
+
+  case "LEAVE":// new case
+  case "leave":
+    window.alert("Leaving the store");
+    break;
+    default:
+      window.alert("You did not pick a valid option. Try again.");
+      shop();
+      break;
+      
+}
 };
 
 //function to end the entire game
@@ -132,7 +190,9 @@ var endGame = function() {
   else {
     window.alert("thank you for playing robot gladiators!. Combe back soon!");
   }
-}
+};
+
+
   
 //start the game when the page loads
 startGame();
